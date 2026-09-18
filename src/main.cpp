5d35ed0a2eb88f140457a54ea4f5848725e73e99
proboom481-dot <proboom481@gmail.com>
2026-09-18 16:57:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/EditorUI.hpp>
 
 using namespace geode::prelude;
 
-$on_mod(Loaded) {
-    log::info("Auto Decor loaded!");
-}
+class $modify(AutoDecorEditorUI, EditorUI) {
+    bool init(LevelEditorLayer* editorLayer) {
+        if (!EditorUI::init(editorLayer))
+            return false;
+
+        auto menu = CCMenu::create();
+        menu->setPosition(0, 0);
+
+        auto buttonSprite = ButtonSprite::create("AUTO DECOR");
+
+        auto button = CCMenuItemSpriteExtra::create(
+            buttonSprite,
+            this,
+            menu_selector(AutoDecorEditorUI::onAutoDecor)
+        );
+
+        button->setPosition(100, 100);
+
+        menu->addChild(button);
+        this->addChild(menu);
+
+        return true;
+    }
+
+    void onAutoDecor(CCObject* sender) {
+        FLAlertLayer::create(
+            "Auto Decor",
+            "Auto Decor button works!",
+            "OK"
+        )->show();
+    }
+};
